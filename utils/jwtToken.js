6875dc8112b692286df17eb1cf6827e6784aa5b0
Kartik Narwal <kartiknarwal38@gmail.com>
2025-09-29 20:58:54 +0530

    changed utils/jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,14 +1,15 @@
-export const sendToken =(user,statusCode,res,message)=>{
-    const token =user.getJWTToken();
-    const options ={
-        expires:new Date(
-            Date.now() + process.env.COOKIE_EXPIRE *24 * 60 * 60 * 1000 
-        ),
-        httpOnly:true,
+export const sendToken = (user, statusCode, res, message) => {
+    const token = user.getJWTToken();
 
+    const options = {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // only true in deployed env
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // important for cross-origin cookies
     };
-    res.status(statusCode).cookie("token" ,token,options).json({
-        success:true,
+
+    res.status(statusCode).cookie("token", token, options).json({
+        success: true,
         user,
         message,
         token,
